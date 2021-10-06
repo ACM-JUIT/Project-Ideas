e474d76fa2e34cf8992b42de6ce5a900e32a5c02
Abhinandan Thakur <73791479+abhinandant34@users.noreply.github.com>
2021-10-06 23:46:36 +0530

    bdab added

--- src/data.js
+++ src/data.js
@@ -1,8 +1,11 @@
 export const projectsData = [
-    {
+   {
       projectName: "Aarhan Khan",
       techStack: "developer of this site",
+    }, 
+    {
+      projectName: "bdab",
+      techStack: "developer of this site",
     },
-
     
   ];
