7769460c8ef83e81820f7acfa7bc5fa1c93434e5
Alisha Siddiqui <92030681+aswritessss@users.noreply.github.com>
2021-10-07 02:16:25 +0530

    added new project
    something wish to work on in order to help people dealing with knee problems

--- src/data.js
+++ src/data.js
@@ -3,6 +3,9 @@ export const projectsData = [
       projectName: "Aarhan Khan",
       techStack: "developer of this site",
     },
-
+     {
+      projectName: "relax on wheels",
+      techStack: "an app to convert chair and bed into wheels and move around. ",
+    },
     
   ];
