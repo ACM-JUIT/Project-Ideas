7b1a72bf674eddf79e92e933bf4b616a8c560e3f
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2021-10-07 09:19:05 +0530

    Bot which behaves just like your friend

--- src/data.js
+++ src/data.js
@@ -3,6 +3,9 @@ export const projectsData = [
       projectName: "Aarhan Khan",
       techStack: "developer of this site",
     },
-
+    {
+      projectName: "Friendot",
+      techStack: "An AI powered bot which intracts with you and talks to you just like your friends",
+    },
     
   ];
