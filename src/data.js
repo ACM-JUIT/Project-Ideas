a8e1f973dcf03923f3d397af28d75fcfdd4811f4
Aditya3403 <adityabansal3403@gmail.com>
2021-10-07 14:12:38 +0530

    Project Ideas

--- src/data.js
+++ src/data.js
@@ -3,6 +3,10 @@ export const projectsData = [
       projectName: "Aarhan Khan",
       techStack: "developer of this site",
     },
+    {
+      projectName: "Task Assistent",
+      techStack: "A AI bot used for telling our to do list for the day,wheather we have a lecture or another important work it would help us manage our work .",
+    },
 
     
   ];
