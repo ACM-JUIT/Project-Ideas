b641b536a6b05a3935ee0265d3991659f3811015
Aayush Girdhar <you@example.com>
2021-10-07 09:43:28 +0530

    added an idea for an app

--- src/data.js
+++ src/data.js
@@ -3,6 +3,10 @@ export const projectsData = [
       projectName: "Aarhan Khan",
       techStack: "developer of this site",
     },
+    {
+      projectName: "Tail Wags",
+      techStack: "An app to solve all your pet's needs.",
+    },
 
     
   ];
