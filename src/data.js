b14eb1d2f2ded0c809ace8de203332ac3455ae61
Siddharth <singhsiddharth6582@gmail.com>
2021-10-23 19:59:43 +0530

    Added project idea : Fast Fingers

--- src/data.js
+++ src/data.js
@@ -1,150 +1,154 @@
 export const projectsData = [
-   {
-      projectName: "Aarhan Khan",
-      techStack: "developer of this site",
-   },
-   {
+  {
+    projectName: "Aarhan Khan",
+    techStack: "developer of this site",
+  },
+  {
     projectName: "Tripsplit",
     techStack: "An app to slipt the trip cost between friends.",
-   },
-    {
-      projectName: "Task Assistent",
-      techStack: "A AI bot used for telling our to do list for the day,wheather we have a lecture or another important work it would help us manage our work .",
-    },
-    {
-      projectName: "bdab",
-      techStack: "developer of this site",
-    },
-    {
-      projectName: "B-fit",
-      techStack: "A application which on recieving your weight and height will suggest the best diet plan and excercises suitable for you",
-    },
-     {
-      projectName: "relax on wheels",
-      techStack: "an app to convert chair and bed into wheels and move around. ",
-    },
-    {
-      projectName: "Friendot",
-      techStack: "An AI powered bot which intracts with you and talks to you just like your friends",
-    },
-  {
-      projectName: "Zarvis",
-      techStack: "Bot Work like an Assitant",
-    },
-   {
-      projectName: "Tail Wags",
-      techStack: "An app to solve all your pet's needs.",
-    },
-  {
-      projectName: "Rythmmy",
-      techStack: "An app dedicated to all the music lovers in which they can create their own music by their own lyrics and beats.",
-    },
-  {
-      projectName: "ROBOME",
-      techStack: "A.I. program that allows to configure basic robot movement controls.",
-    },
-    {
-      projectName: "How u doin?",
-      techStack: "An application which tells you about the disease you might have based on your symptoms and suggest some specialists near you.",
-    },
-  {
-      projectName: "Mind Focusing",
-      techStack: "An App which helps you to focus on thing in which you want to focus, Shows you relaxing Wallpapers and also have a Pomodoro clock which helps you start your sessions to Learn new things",
-    }, 
-   {
-      projectName: "Calm",
-      techStack: "An app to help you meditate",
-    },
-    {
-      projectName: "liscence",
-      techStack: "An app to tell you are elligible for driving or not",
-    },
-      {
-      projectName: "Easy Learn",
-      techStack: "an app that tracks assignments and provides learning material related to them",
-      }, 
-    {
-      projectName: "LearnGamesWithMe",
-      techStack: "an app that will have important key functions ,Commands, Tactics written for various online games and several tactics ,positions for offline games as well.",
-      },
-      {
-        projectName: "A.D.D",
-        techStack: "Assistant for Drowsy Driving : It will observe driver's behaviour and will stop vehicle and alarm driver about his drowsy driving , it will coordinate with EV's software to change music type to keep driver awake and enable auto pilot.",
-     },
-  {
-      projectName: "Doorbell cum Visitor Indicator",
-      techStack: "This doorbell cum visitor indicator circuit can give identification of the visitor to your home in your absence",
-    },
-   {
-      projectName: "Alcohol Detectors",
-      techStack: "This device will sense the consumption of alcohol by the drivers and will correspondingly limit the max speed of car in order to avoid accidents",
-   },
- {
-      projectName: "Alert",
-      techStack: "An app tells you what to do during a disaster.",
-    },
-{
-      projectName: "3d-Navigation",
-      techStack: "An app that shows real time navigation,traffic updates with video navigation",
-    },
-  {
-      projectName: "task reminder",
-      techStack: "This app helps you to track your time",
-    },
-   {
-      projectName: "dairy car",
-      techStack: "Distribute milk through IOT",
-    },
-     {
-      projectName: "Calculator",
-      techStack: "MERN",
-    },
-    {
-      projectName: "Follow me",
-      techStack: "An app that connects your vehicle to mobile phones of your family members to keep a track on you for your safety or in case someone is missing",
-    },
-   {
-      projectName: "one eye",
-      techStack: "AI program connected to building security cameras capable to identify people using face recognition and barcode scanning and track intruders",
-    },
-  {
-      projectName: "Farm weeder",
-      techStack: "weeding robots with elixier",
-    },
-  {
-      projectName: "advanced alarm clock",
-      techStack: "an alarm that will give you maths problems in morning and it will not stop ringing until the person solves it, so you wake up in morning.",
-   },
-   {
-    projectName: "Classroom with Video Chat",
-    techStack:"React for frontend. And Node.JS, Express.JS, and MongoDB for backend."
-   },
-{
-      projectName: "Alpha",
-      techStack: "An AI which can find anyone , anytime , anywhere in the world by just typing his name and showing his/her picture to ALPHA",
-   },
-   {
-      projectName: "Furnitioner",
-      techStack: "FLUTTER Android App for auctioning old furnitures",
-   }, 
-     {
-   projectName: "Music app recommender system",
-    techStack:"Creating a filtering system for songs in a music appliction by applying Machine Learning algorithms"
-     },
-   {
-      projectName: "Clothes collection",
-    techStack:"Flutter. An AI based flutter application that suggests clothes as per user reference."
-   },
-{
-      projectName: "Fitness Trainer App",
-      techStack: "This app helps you with your daily exercises and helps you boost your stamina.",
-   },
-   {
-    projectName: "Sorting Alorithm Visualizer",
-    techStack:"An interactive visualizer built using Javascript to help graphically visualize multiple sorting algorithms."
   },
-{
-      projectName: "Know your Date",
-      techStack: "dating app with a personality test that can help you filter people ",
-    },
+  {
+    projectName: "Task Assistent",
+    techStack: "A AI bot used for telling our to do list for the day,wheather we have a lecture or another important work it would help us manage our work .",
+  },
+  {
+    projectName: "bdab",
+    techStack: "developer of this site",
+  },
+  {
+    projectName: "B-fit",
+    techStack: "A application which on recieving your weight and height will suggest the best diet plan and excercises suitable for you",
+  },
+  {
+    projectName: "relax on wheels",
+    techStack: "an app to convert chair and bed into wheels and move around. ",
+  },
+  {
+    projectName: "Friendot",
+    techStack: "An AI powered bot which intracts with you and talks to you just like your friends",
+  },
+  {
+    projectName: "Zarvis",
+    techStack: "Bot Work like an Assitant",
+  },
+  {
+    projectName: "Tail Wags",
+    techStack: "An app to solve all your pet's needs.",
+  },
+  {
+    projectName: "Rythmmy",
+    techStack: "An app dedicated to all the music lovers in which they can create their own music by their own lyrics and beats.",
+  },
+  {
+    projectName: "ROBOME",
+    techStack: "A.I. program that allows to configure basic robot movement controls.",
+  },
+  {
+    projectName: "How u doin?",
+    techStack: "An application which tells you about the disease you might have based on your symptoms and suggest some specialists near you.",
+  },
+  {
+    projectName: "Mind Focusing",
+    techStack: "An App which helps you to focus on thing in which you want to focus, Shows you relaxing Wallpapers and also have a Pomodoro clock which helps you start your sessions to Learn new things",
+  },
+  {
+    projectName: "Calm",
+    techStack: "An app to help you meditate",
+  },
+  {
+    projectName: "liscence",
+    techStack: "An app to tell you are elligible for driving or not",
+  },
+  {
+    projectName: "Easy Learn",
+    techStack: "an app that tracks assignments and provides learning material related to them",
+  },
+  {
+    projectName: "LearnGamesWithMe",
+    techStack: "an app that will have important key functions ,Commands, Tactics written for various online games and several tactics ,positions for offline games as well.",
+  },
+  {
+    projectName: "A.D.D",
+    techStack: "Assistant for Drowsy Driving : It will observe driver's behaviour and will stop vehicle and alarm driver about his drowsy driving , it will coordinate with EV's software to change music type to keep driver awake and enable auto pilot.",
+  },
+  {
+    projectName: "Doorbell cum Visitor Indicator",
+    techStack: "This doorbell cum visitor indicator circuit can give identification of the visitor to your home in your absence",
+  },
+  {
+    projectName: "Alcohol Detectors",
+    techStack: "This device will sense the consumption of alcohol by the drivers and will correspondingly limit the max speed of car in order to avoid accidents",
+  },
+  {
+    projectName: "Alert",
+    techStack: "An app tells you what to do during a disaster.",
+  },
+  {
+    projectName: "3d-Navigation",
+    techStack: "An app that shows real time navigation,traffic updates with video navigation",
+  },
+  {
+    projectName: "task reminder",
+    techStack: "This app helps you to track your time",
+  },
+  {
+    projectName: "dairy car",
+    techStack: "Distribute milk through IOT",
+  },
+  {
+    projectName: "Calculator",
+    techStack: "MERN",
+  },
+  {
+    projectName: "Follow me",
+    techStack: "An app that connects your vehicle to mobile phones of your family members to keep a track on you for your safety or in case someone is missing",
+  },
+  {
+    projectName: "one eye",
+    techStack: "AI program connected to building security cameras capable to identify people using face recognition and barcode scanning and track intruders",
+  },
+  {
+    projectName: "Farm weeder",
+    techStack: "weeding robots with elixier",
+  },
+  {
+    projectName: "advanced alarm clock",
+    techStack: "an alarm that will give you maths problems in morning and it will not stop ringing until the person solves it, so you wake up in morning.",
+  },
+  {
+    projectName: "Classroom with Video Chat",
+    techStack: "React for frontend. And Node.JS, Express.JS, and MongoDB for backend."
+  },
+  {
+    projectName: "Alpha",
+    techStack: "An AI which can find anyone , anytime , anywhere in the world by just typing his name and showing his/her picture to ALPHA",
+  },
+  {
+    projectName: "Furnitioner",
+    techStack: "FLUTTER Android App for auctioning old furnitures",
+  },
+  {
+    projectName: "Music app recommender system",
+    techStack: "Creating a filtering system for songs in a music appliction by applying Machine Learning algorithms"
+  },
+  {
+    projectName: "Clothes collection",
+    techStack: "Flutter. An AI based flutter application that suggests clothes as per user reference."
+  },
+  {
+    projectName: "Fitness Trainer App",
+    techStack: "This app helps you with your daily exercises and helps you boost your stamina.",
+  },
+  {
+    projectName: "Sorting Alorithm Visualizer",
+    techStack: "An interactive visualizer built using Javascript to help graphically visualize multiple sorting algorithms."
+  },
+  {
+    projectName: "Know your Date",
+    techStack: "dating app with a personality test that can help you filter people ",
+  },
+  {
+    projectName: "Fast Fingers",
+    techStack: "An interactive typing game that projects random words on screen. With every correct word entered within the time limit, the score increments. ",
+  },
 ];
