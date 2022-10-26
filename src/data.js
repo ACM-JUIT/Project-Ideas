5392eeba1937f1df838c969ac874d2383a91cfbc
Aditya <aadityasaini8008@gmail.com>
2022-10-27 00:15:55 +0530

    first commit

--- src/data.js
+++ src/data.js
@@ -90,5 +90,9 @@ export const projectsData = [
   {
     projectName: "All in One OTT",
     techStack: "Brings media content from every OTT platform into one.",
-  }
+  },
+  {
+    projectName: "fitness APP",
+    techStack: "An app that records fitness activities.",
+  },
 ]
